0e5b1200083b0711b9f71d11c09f9e8ff74b9fee
Brittany Fortenberry <95709599+bfortenberry22@users.noreply.github.com>
2022-01-15 17:45:41 -0600

    cases defined

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,13 @@
 var welcome = function(){
     window.alert("Welcome to Password Generator. This program will generate a password based on your set criteria.");
     passLength();
-
+}
 //Asked user about lowercase, uppercase, numberical and special characters
 var passLength = function(){
     length = window.prompt("How many characters would you like in the password. Please choose a number between 8 and 128.");
     length = parseInt (length) //converts desired length from a string
     console.log(length + " characters");
-   
+}  
 var ask = function(){
     var lower = window.confirm("Would you like your password to include LOWERCASE letters?");
   
@@ -54,6 +54,7 @@ var ask = function(){
         ask();
     }
 }
+
 //Create Password
 var generatePassword = function(charType){
     var result = " ";
@@ -64,5 +65,22 @@ var generatePassword = function(charType){
     }
 
     console.log(result);
-    window.alert("Your new password is " + result  )
+    window.alert("Your new password is " + result)
 }
+
+/*CHARACTER BANK*/
+var luns = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%^&*.?/";
+var lun = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!123456789";
+var lus = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*.?/";
+var lns = "abcdefghijklmnopqrstuvwxyz123456789!@#$%^&*.?/";
+var uns = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%^&*.?/";
+var lu = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var ln = "abcdefghijklmnopqrstuvwxyz123456789";
+var ls = "abcdefghijklmnopqrstuvwxyz!@#$%^&*.?/";
+var un = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
+var us = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*.?/";
+var ns = "123456789!@#$%^&*.?/";
+var l = "abcdefghijklmnopqrstuvwxyz";
+var u = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var n = "123456789";
+var s = "!@#$%^&*.?/";
