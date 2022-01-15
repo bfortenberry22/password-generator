86a6f459d4b69edf28aeb1d4aebc2f88059484aa
Brittany Fortenberry <95709599+bfortenberry22@users.noreply.github.com>
2022-01-15 17:33:57 -0600

    initial code

--- assets/script.js
+++ assets/script.js
@@ -7,15 +7,7 @@ var welcome = function(){
 var passLength = function(){
     length = window.prompt("How many characters would you like in the password. Please choose a number between 8 and 128.");
     length = parseInt (length) //converts desired length from a string
-    console.log(length + " characters");
-    if (length >= 8 && length <=128){
-      ask();
-    } else {
-        window.alert("Please choose a password length between 8 and 128.")
-        passLength();
-    }
-    return length;
-}
+   
 var ask = function(){
     var lower = window.confirm("Would you like your password to include LOWERCASE letters?");
   
@@ -25,42 +17,7 @@ var ask = function(){
    
     var speci = window.confirm("Would you like your password to include SPECIAL CHARACTERS, such as ! @ # $ % ^ & *");
 
-    //listed the different combinations of the choices ending with an else statement that will loop the function if no choices are made
-    if (lower && upper && num && speci){
-        generatePassword (luns);
-    } else if (lower && upper && num){
-        generatePassword(lun);
-    } else if (lower && upper&& speci){
-        generatePassword(lus);
-    }else if (lower && num && speci){
-        generatePassword(lns);
-    }else if (upper && num && speci){
-        generatePassword(uns);    
-    } else if (lower && upper){
-        generatePassword (lu);
-    } else if (lower && num ){
-        generatePassword(ln);
-    } else if (lower && speci){
-        generatePassword(ls);
-    } else if (upper && num){
-        generatePassword(un);
-    }else if (upper && speci){
-        generatePassword(us); 
-    }else if (num && speci){
-        generatePassword(ns);
-    }else if (upper){
-        generatePassword(u);
-    }else if (lower){
-        generatePassword(l);
-    }else if (num){
-        generatePassword(n);
-    } else if (speci){
-        generatePassword(s)
-    }else{
-        window.alert("Please choose at least one type of characters for the password.")
-        ask();
-    }
-}
+   
 //Create Password
 var generatePassword = function(charType){
     var result = " ";
