09a269f4ea63a2b7baa20e2eb86dda2964449f80
Brittany Fortenberry <95709599+bfortenberry22@users.noreply.github.com>
2022-01-15 17:42:12 -0600

    casses added

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 var welcome = function(){
     window.alert("Welcome to Password Generator. This program will generate a password based on your set criteria.");
     passLength();
-
+}
 //Asked user about lowercase, uppercase, numberical and special characters
 var passLength = function(){
     length = window.prompt("How many characters would you like in the password. Please choose a number between 8 and 128.");
@@ -73,24 +73,24 @@ var generatePassword = function(charType){
     console.log(result);
     window.alert("Your new password is " + result  )
 
-}
+};
 
 
 /*CHARACTER BANK*/
-var luns = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%^&*.?/"
-var lun = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!123456789"
-var lus = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*.?/"
-var lns = "abcdefghijklmnopqrstuvwxyz123456789!@#$%^&*.?/"
-var uns = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%^&*.?/"
-var lu = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var ln = "abcdefghijklmnopqrstuvwxyz123456789"
-var ls = "abcdefghijklmnopqrstuvwxyz!@#$%^&*.?/"
-var un = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789"
-var us = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*.?/"
-var ns = "123456789!@#$%^&*.?/"
-var l = "abcdefghijklmnopqrstuvwxyz"
-var u = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var n = "123456789"
-var s = "!@#$%^&*.?/"
+var luns = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%^&*.?/";
+var lun = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!123456789";
+var lus = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*.?/";
+var lns = "abcdefghijklmnopqrstuvwxyz123456789!@#$%^&*.?/";
+var uns = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%^&*.?/";
+var lu = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var ln = "abcdefghijklmnopqrstuvwxyz123456789";
+var ls = "abcdefghijklmnopqrstuvwxyz!@#$%^&*.?/";
+var un = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
+var us = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*.?/";
+var ns = "123456789!@#$%^&*.?/";
+var l = "abcdefghijklmnopqrstuvwxyz";
+var u = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var n = "123456789";
+var s = "!@#$%^&*.?/";
 
 
